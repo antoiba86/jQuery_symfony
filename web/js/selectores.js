eb8b14c3c38cffef304e0ed02560def91df86e4c
Anto <Anto@Anto-PC>
2017-01-05 00:38:25 +0100

    Finished introduction html. Added selectors content. Added style for selectors. Added selector js.

---
+++ web/js/selectores.js
@@ -0,0 +1,69 @@
+$(function() {
+   $(".odd-li").click(function() {
+       $("p, li").css("background-color", "transparent");
+       $("li:odd").css( "background-color", "red"); 
+   });
+   $(".odd-p").click(function() {
+       $("p, li").css("background-color", "transparent");
+       $("p:odd").css( "background-color", "red"); 
+   });
+   $(".even-li").click(function() {
+       $("p, li").css("background-color", "transparent");
+       $("li:even").css( "background-color", "red"); 
+   });
+   $(".even-p").click(function() {
+       $("p, li").css("background-color", "transparent");
+       $("p:even").css( "background-color", "red"); 
+   });
+   $(".clear-oddeven").click(function() {
+       $("p, li").css("background-color", "transparent");
+   });
+});
+
+$(function() {
+   $(".first-li").click(function() {
+       $(".seleccion").css("background-color", "transparent");
+       $(".seleccion:first").css( "background-color", "red"); 
+   });
+   $(".eq-li").click(function() {
+       var valor = $("#valorSelect").val();
+       $(".seleccion").css("background-color", "transparent");
+       $(".seleccion:eq("+valor+")").css( "background-color", "red"); 
+   });
+   $(".gt-li").click(function() {
+       var valor = $("#valorSelect").val();
+       $(".seleccion").css("background-color", "transparent");
+       $(".seleccion:gt("+valor+")").css( "background-color", "red"); 
+   });
+   $(".lt-li").click(function() {
+       var valor = $("#valorSelect").val();
+       $(".seleccion").css("background-color", "transparent");
+       $(".seleccion:lt("+valor+")").css( "background-color", "red"); 
+   });
+   $(".slice-li").click(function() {
+       var valor = $("#valorSelect").val();
+       var valor2 = $("#valorSelect2").val();
+       $(".seleccion").css("background-color", "transparent");
+       $(".seleccion").slice(valor, valor2).css( "background-color", "red"); 
+   });
+   $(".clear-li").click(function() {
+       $(".seleccion").css("background-color", "transparent");
+   });
+});
+
+$(function() {
+   $(".tell-hidden").click(function() {
+       var num = $(".seleccion2:hidden");
+       alert("Los elementos escondidos son " + num.length);
+   });
+   $(".show-hidden").click(function() {
+       $(".seleccion2:hidden").show();
+   });
+   $(".all-hidden").click(function() {
+       $(".seleccion2").hide();
+   });
+   $(".clear-li").click(function() {
+       $(".seleccion2").show();
+       $(".element-hidden").hide();
+   });
+});
