3f0f5f1986752f10a68bbadcd6e3277654a48347
Anto <Anto@Anto-PC>
2017-01-06 01:44:34 +0100

    Added starwars ancestor. Added form selectors.

--- web/js/selectores.js
+++ web/js/selectores.js
@@ -80,3 +80,114 @@ $(function() {
        $("#padre-div").css("background-color", "transparent");
    });
 });
+
+$(function() {
+   $(".shmi-son").click(function() {
+       clear4();
+       $( ".shmi-child>img" ).css( "border", "3px solid red" );
+   });
+   $(".vader-children").click(function() {
+       clear4();
+       $( ".vader-child>img" ).css( "border", "3px solid red" );
+   });
+   $(".leia-son").click(function() {
+       clear4();
+       $( ".leia-child>img" ).css( "border", "3px solid red" );
+   });
+   $(".shmi-descendant").click(function() {
+       clear4();
+       $( ".shmi-child img" ).css( "border", "3px solid red" );
+   });
+   $(".vader-descendant").click(function() {
+       clear4();
+       $( ".vader-child img" ).css( "border", "3px solid red" );
+   });
+   $(".luke-siblings").click(function() {
+       clear4();
+       $( "#luke ~ div" ).find(".ancestor").css( "border", "3px solid red" );
+   });
+   $(".kylo-mother").click(function() {
+       clear4();
+       $( "#kylo" ).parent().find(".ancestor").css( "border", "3px solid red" );
+   });
+   $(".kylo-ancestor").click(clear4,function() {
+       clear4();
+       $( "#kylo img" ).parentsUntil("#star-wars").find( ".ancestor" ).css( "border", "3px solid red" );
+   });
+   $(".clear-li4").click(clear4);
+});
+
+function clear4() {
+    $("#star-wars img").css("border", "3px transparent");
+    $("#star-wars div").css("border", "3px transparent");
+}
+
+$(function() {
+   $(".input-type").click(function() {
+        var allInputs = $( ":input");
+        $( "#form-result" ).text( "Encontrados " + allInputs.length + " inputs." );
+   });
+   $(".pass-type").click(function() {
+       var input = $( "input:password" ).css({
+        background: "yellow",
+        border: "3px red solid"
+      });
+      $( "#form-result" )
+        .text( "Hay " + input.length + " input de tipo 'password'." )
+        .css( "color", "red" );
+   });
+   $(".reset-type").click(function() {
+       var input = $( "input:reset" ).css({
+        background: "yellow",
+        border: "3px red solid"
+      });
+      $( "#form-result" )
+        .text( "Hay " + input.length + " input de tipo 'reset'." )
+        .css( "color", "red" );
+   });
+   $(".radio-type").click(function() {
+       var input = $( "input:radio" ).css({
+        background: "yellow",
+        border: "3px red solid"
+      });
+      $( "#form-result" )
+        .text( "Hay " + input.length + " input de tipo 'radiobutton'." )
+        .css( "color", "red" );
+   });
+   $(".submit-type").click(function() {
+       var input = $( "input:submit" ).css({
+        background: "yellow",
+        border: "3px red solid"
+      });
+      $( "#form-result" )
+        .text( "Hay " + input.length + " input de tipo 'submit'." )
+        .css( "color", "red" );
+   });
+   $(".text-type").click(function() {
+       var input = $( "input:text" ).css({
+        background: "yellow",
+        border: "3px red solid"
+      });
+      $( "#form-result" )
+        .text( "Hay " + input.length + " input de tipo 'text'." )
+        .css( "color", "red" );
+   });
+   $(".checkbox-type").click(function() {
+       var input = $( "input:checkbox" ).css({
+        background: "yellow",
+        border: "3px red solid"
+      });
+      $( "#form-result" )
+        .text( "Hay " + input.length + " input de tipo 'checkbox'." )
+        .css( "color", "red" );
+   });
+   $(".button-type").click(function() {
+       var input = $( "form input:button" ).css({
+        background: "yellow",
+        border: "3px red solid"
+      });
+      $( "#form-result" )
+        .text( "Hay " + input.length + " input de tipo 'button'." )
+        .css( "color", "red" );
+   });
+});
